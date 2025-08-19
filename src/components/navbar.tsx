6bccb022bcf2ce7277561e9f477b991e1aa8f0c0
Akmal Khan <akmalnawabi007@gmail.com>
2025-08-19 14:57:33 +0330

    new

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { SignInButton, SignUpButton } from "@clerk/nextjs"
+import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs"
 import { Kanban } from "lucide-react"
 import { Button } from "./ui/button"
 
 export default function Navbar() {
+    const { isSignedIn, user } = useUser()
     return (
         <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
             <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
@@ -12,16 +13,19 @@ export default function Navbar() {
                     <Kanban className="w-6 h-6 sm:h-8 sm:w-8 text-blue-500" />
                     <span className="texl-xl sm:text-2xl font-bold text-gray-900">Akmal Nawabi</span>
                 </div>
-
                 <div className="flex items-center space-x-2">
-                    <div>
+                    {isSignedIn ? <div className="flex flex-col sm:flex-row 
+                    items-end sm:items-center space-y-1 sm:space-y-0 sm:space-x-4">
+                        <span className="text-sm sm:text-base text-gray-600">Welcome, {user?.firstName ?? user?.emailAddresses[0].emailAddress}</span>
+                        
+                    </div> : <div>
                         <SignInButton>
                             <Button variant="ghost">Sign In</Button>
                         </SignInButton>
                         <SignUpButton>
                             <Button>Sign Up</Button>
                         </SignUpButton>
-                    </div>
+                    </div>}
                 </div>
             </div>
         </header>

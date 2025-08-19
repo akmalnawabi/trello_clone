3fc42450dc0b7cec88b352013021cbd8f6d975c2
Akmal Khan <akmalnawabi007@gmail.com>
2025-08-19 14:37:48 +0330

    new

---
+++ src/components/navbar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { SignInButton, SignUpButton } from "@clerk/nextjs"
+import { Kanban } from "lucide-react"
+import { Button } from "./ui/button"
+
+export default function Navbar() {
+    return (
+        <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+            <div className="container mx-auto px-4 py-3 sm:py-4 flex items-center justify-between">
+                <div className="flex items-center space-x-2">
+                    <Kanban className="w-6 h-6 sm:h-8 sm:w-8 text-blue-500" />
+                    <span className="texl-xl sm:text-2xl font-bold text-gray-900">Akmal Nawabi</span>
+                </div>
+
+                <div className="flex items-center space-x-2">
+                    <div>
+                        <SignInButton>
+                            <Button variant="ghost">Sign In</Button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <Button>Sign Up</Button>
+                        </SignUpButton>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
